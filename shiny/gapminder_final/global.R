459beba9b19003b70ee619efa85c107aa8390c63
Jayaprakash-1979 <jayaprakashu@packt.com>
2025-01-06 19:23:36 +0530

    Code files Updated

--- shiny/gapminder_final/global.R
+++
@@ -1,9 +0,0 @@
-library(ggplot2)
-library(dplyr)
-library(tidyr)
-library(gapminder)
-
-year_start <- gapminder$year %>% min()
-year_end <- gapminder$year %>% max()
-
-continents <- gapminder$continent %>% unique()
